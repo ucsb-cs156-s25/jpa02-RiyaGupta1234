760afc89d98587e6bef896f0bd0996b8040d42e1
Riya Gupta <riyagupta@csil.cs.ucsb.edu>
2025-04-08 10:17:01 -0700

    full line/mutation coverage

--- src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
@@ -11,7 +11,7 @@ public class TeamTest {
 
     @BeforeEach
     public void setup() {
-        team = new Team("test-team");    
+        team = new Team("test-team");   
     }
 
     @Test
@@ -22,5 +22,68 @@ public class TeamTest {
    
     // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
     // 100% mutation coverage (all mutants timed out or killed)
+
+    @Test
+    public void toString_returns_correct_string() {
+        assertEquals("Team(name=test-team, members=[])", team.toString());
+    }
+
+    @Test
+    public void equals_case_1_check() {
+        assertEquals(team.equals(team), true);
+    }
+
+    @Test
+    public void equals_case_2_check() {
+        Object obj = new Object();
+        assertEquals(team.equals(obj), false);
+    }
+
+    @Test
+    public void equals_case_3_same_name_and_mem() {
+        Team teamsame = new Team();
+        teamsame.setName("test-team");
+        assertEquals(team.equals(teamsame), true);
+    }
+
+    @Test
+    public void equals_case_3_same_name_diff_mem() {
+        Team nameSameMemDiff = new Team();
+        nameSameMemDiff.setName("test-team");
+        nameSameMemDiff.addMember("member_diff");
+        assertEquals(team.equals(nameSameMemDiff), false);
+    }
+
+    @Test
+    public void equals_case_3_diff_name_same_mem() {
+        Team nameDiffMemSame = new Team();
+        nameDiffMemSame.setName("test-team-diff");
+        assertEquals(team.equals(nameDiffMemSame), false);
+    }
+
+    @Test
+    public void equals_case_3_diff_name_diff_mem() {
+        Team allDiff = new Team();
+        allDiff.setName("test-team-diff");
+        allDiff.addMember("member-diff");
+        assertEquals(team.equals(allDiff), false);
+    }
+
+    @Test
+    public void hash_match() {
+        // Team t1 = new Team();
+        // t1.setName("foo");
+        // t1.addMember("bar");
+        // Team t2 = new Team();
+        // t2.setName("foo");
+        // t2.addMember("bar");
+        // assertEquals(t1.hashCode(), t2.hashCode());
+
+        Team t = new Team();
+        int result = t.hashCode();
+        int expectedResult = 1;
+        assertEquals(expectedResult, result);
+    }
+
 
 }
